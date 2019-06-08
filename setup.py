f2e23dafc46e54d18a556b6fe75e7d5264b2ca06
Drew J. Sonne <drew.sonne@gmail.com>
2019-06-08 09:47:13 +0100

    Add skeleton

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pybloods',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Drew J. Sonne',
+    author_email='',
+    description='',
+    install_requires=['connexion']
+)
